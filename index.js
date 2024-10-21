b5bf3e4414f0c96e9ff9b645037f966cd2ca5ef2
niraop <niralibert@gmail.com>
2024-10-21 07:06:46 +0530

    2nd Commit
    I have commented the postgresql code to connect to the database so it doesn't have any database

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import pg from "pg"
-import axios from 'axios';
+/*import pg from "pg"
+import axios from 'axios';*/
 
 const app = express();
 const PORT = 3000;
 
-const db = new pg.Client({
+/*const db = new pg.Client({
     user: "postgres",
     host: "localhost",
     database: "Portfolio website",
     password: "simplesql",
     port: 5432,
 })
-db.connect()
+db.connect()*/
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
@@ -24,7 +24,7 @@ app.get('/',(req,res)=>{
     })
 })
 
-app.post('/submitContact', async (req,res)=>{
+/*app.post('/submitContact', async (req,res)=>{
     const email = req.body.email
     const comments = req.body.comment
 
@@ -36,7 +36,7 @@ app.post('/submitContact', async (req,res)=>{
         res.status(500).send('An error occurred while saving your comment.');
     }
 
-})
+})*/
 
 app.listen(PORT, () =>{
     console.log("Litening to the server");
